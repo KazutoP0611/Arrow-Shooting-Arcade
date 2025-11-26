d48db6b7aa7e68f8b8595782c9568b65af8d228e
PPYukito <PanutatR@gmail.com>
2025-11-26 17:36:32 +0900

    [Mod] Creating shooting arrow from player in 3rd person perspective; (not yet complete)

--- Assets/Scripts/ArrowShooter.cs
+++ Assets/Scripts/ArrowShooter.cs
@@ -6,9 +6,21 @@ public class ArrowShooter : MonoBehaviour
     [SerializeField] private Transform shootPoint;
     [SerializeField] private GameObject arrowPrefab;
 
+    private Vector3 shootDirection;
+
     public void ShootArrow()
     {
         GameObject arrowRigid = Instantiate(arrowPrefab, shootPoint.position, Quaternion.identity);
-        arrowRigid.GetComponent<Rigidbody>().AddForce(shootPoint.transform.forward * arrowForce, ForceMode.Impulse);
+
+        Vector3 tempDirection = shootPoint.position;
+        tempDirection.y = Camera.main.transform.position.y;
+        shootDirection = Camera.main.transform.forward - tempDirection;
+        arrowRigid.GetComponent<Rigidbody>().AddForce(shootPoint.forward * arrowForce, ForceMode.Impulse);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(shootPoint.position, shootDirection);
     }
 }

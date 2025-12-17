da7cf5827c6f705db052ee93fd8cad5a98c6bd1a
PPYukito <PanutatR@gmail.com>
2025-12-17 17:11:24 +0900

    [Mod] Finished screen transition and panels open after game end or time out;

--- Assets/Scripts/GameplayScene/ResultScreenController.cs
+++ Assets/Scripts/GameplayScene/ResultScreenController.cs
@@ -1,12 +1,26 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
 public class ResultScreenController : MonoBehaviour
 {
+    [SerializeField] private Fading resultScreenFade;
+
+    private Coroutine resultFadeCo;
+
     //[SerializeField] private TextMeshProUGUI result
     public void ShowGameResult()
     {
+        if (resultFadeCo != null)
+            StopCoroutine(resultFadeCo);
+        StartCoroutine(ResultFadeCoroutine());
+    }
 
+    IEnumerator ResultFadeCoroutine()
+    {
+        resultScreenFade.FadeIn(1.4f);
+
+        yield return resultScreenFade.fadeCoroutine;
     }
 
     public void ShowResult(int points, int timeLeft)
